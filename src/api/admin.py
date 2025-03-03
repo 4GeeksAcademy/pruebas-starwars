1100813c1bf58a9389db7a387c7f0d622a19940c
JaimeGHE <jaimeghe2@gmail.com>
2025-03-03 10:11:21 +0000

    Relaciones

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,16 +1,20 @@
 import os
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
-from .models import db, Users, Posts, Followers, Media, Comments
-
+from models import db, Users, Posts, Comments, Followers, Media, Planet, Starship, Character, FavoritePlanets, FavoriteStarships, FavoriteCharacters
 
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
     app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
     admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap3')
-    # Add your models here, for example this is how we add a the User model to the admin
-    admin.add_view(ModelView(Users, db.session))  # You can duplicate that line to add mew models
+    admin.add_view(ModelView(Users, db.session))
     admin.add_view(ModelView(Posts, db.session))
     admin.add_view(ModelView(Followers, db.session))
-    admin.add_view(ModelView(Media, db.session))
     admin.add_view(ModelView(Comments, db.session))
+    admin.add_view(ModelView(Media, db.session))
+    admin.add_view(ModelView(Planet, db.session))
+    admin.add_view(ModelView(Starship, db.session))
+    admin.add_view(ModelView(Character, db.session))
+    admin.add_view(ModelView(FavoritePlanets, db.session))
+    admin.add_view(ModelView(FavoriteStarships, db.session))
+    admin.add_view(ModelView(FavoriteCharacters, db.session))
--- src/api/models.py
+++ src/api/models.py
@@ -9,23 +9,66 @@ class Users(db.Model):
     lastname = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
 
+    def __repr__(self):
+        return f'<Users {self.email}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "firstname": self.firstname,
+            "lastname": self.lastname,
+            "email": self.email
+        }
+
 class Posts(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user = db.relationship('Users', backref=db.backref('posts', lazy=True))
+
+    def __repr__(self):
+        return f'<Posts {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id
+        }
 
 class Comments(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     comment_text = db.Column(db.String(255), nullable=False)
-    author_id = db.Column(db.Integer, nullable=False)
-    post_id = db.Column(db.Integer, nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable=False)
+
+    author = db.relationship('Users', backref=db.backref('comments', lazy=True))
+    post = db.relationship('Posts', backref=db.backref('comments', lazy=True))
+
+    def __repr__(self):
+        return f'<Comments {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "comment_text": self.comment_text,
+            "author_id": self.author_id,
+            "post_id": self.post_id
+        }
 
 class Followers(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_from_id = db.Column(db.Integer, nullable=False)
-    user_to_id = db.Column(db.Integer, nullable=False)
+    user_from_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user_to_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
-class Media(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    type = db.Column(db.String(10), nullable=False)
-    url = db.Column(db.String(255), nullable=False)
-    post_id = db.Column(db.Integer, nullable=False)
+    user_from = db.relationship('Users', foreign_keys=[user_from_id], backref=db.backref('following', lazy=True))
+    user_to = db.relationship('Users', foreign_keys=[user_to_id], backref=db.backref('followers', lazy=True))
+
+    def __repr__(self):
+        return f'<Followers {self.user_from_id} follows {self.user_to_id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_from_id": self.user_from_id,
+            "user_to_id": self.user_to_id
+        }
